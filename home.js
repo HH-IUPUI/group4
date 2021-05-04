dc017f8bee07e72ee8d2a49a97f05140b0d9f295
AlRemes <78789352+AlRemes@users.noreply.github.com>
2021-05-04 20:13:44 +0300

    deleted unused files and change class footer_container to footer_top for every page

--- home.js
+++
@@ -1,9 +0,0 @@
-function show(){
-    var show = document.getElementsByClassName("positiveCollapse");
-
-    if (show[0].style.display === "none"){
-        show[0].style.display = "block";
-    } else{
-        show[0].style.display = "none";
-    }
-}
